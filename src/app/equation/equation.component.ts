e379fcaeea259f97aaaff29451327bb83b3dff7d
steingartpeter <steingart.p2@gmail.com>
2023-02-12 16:09:39 +0100

    2023-02-12 001

---
+++ src/app/equation/equation.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-equation',
+  templateUrl: './equation.component.html',
+  styleUrls: ['./equation.component.css'],
+})
+export class EquationComponent implements OnInit {
+  mathForm = new FormGroup({
+    a: new FormControl(this.rndNumber()),
+    b: new FormControl(this.rndNumber()),
+    answer: new FormControl(''),
+  });
+
+  constructor() {}
+
+  get a() {
+    return this.mathForm.value.a;
+  }
+  get b() {
+    return this.mathForm.value.b;
+  }
+
+  ngOnInit(): void {}
+
+  rndNumber() {
+    return Math.floor(Math.random() * 20);
+  }
+}
